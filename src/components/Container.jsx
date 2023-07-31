6661251491f10761c90a58d12b31421e93f93769
anchalraj31082004 <anchalraj20045@gmail.com>
2023-07-31 12:16:58 +0530

    updated the UI of the app

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -10,11 +10,11 @@ const Container = () => {
     <>
       {loader ? (
         <div className="flex gap-5 flex-col items-center justify-center">
-          <img className="w-full h-full" src="https://media.tenor.com/1zUYT3BN2tYAAAAj/skirt-bunny-girl.gif"/>
-          <p className="text-2xl font-medium text-slate-200"> --- Content is Loading --- </p>
+          <img className="w-[200px] h-[200px]" src="https://media.tenor.com/hz61LSrgeI0AAAAi/squall-scoll.gif"/>
+          <p className="text-xl font-medium text-slate-200"> --- Content is Loading --- </p>
         </div>
       ) : (
-        <div className="flex flex-col gap-1 text-slate-200 h-[85%] w-[65%] opacity-70 backdrop-blur-2xl shadow-2xl bg-gradient-to-br from-slate-600 to-slate-80 rounded-xl">
+        <div className="flex flex-col gap-1 text-slate-200 h-[85%] w-[65%] backdrop-filter backdrop-blur-xl shadow-2xl bg-gradient-to-br from-slate-600 to-slate-80 rounded-2xl">
           <h1 className="text-4xl w-fit text-sky-900 font-bold p-4 rounded-br-lg bg-slate-200 hover:bg-orange-300 ">
             CLOUD SEEKER
           </h1>
